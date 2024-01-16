1aba5bf8ef7651688ca9ac4684b9e2df92b5bf47
Alexis Perez <alexisperez@Alexiss-MacBook-Air.local>
2024-01-16 01:40:43 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import './App.css'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import Navbar from './components/NavBar/Navbar'
-import { ChakraProvider, Grid,  } from '@chakra-ui/react'
-
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Navbar from './components/NavBar/Navbar';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
-
   return (
     <ChakraProvider>
-      <Grid templateColumns='repeat(3, 1fr)' gap={5}>
-      <Navbar title={'The Grinch'}/>
-      </Grid>
-      <ItemListContainer greeting={'Welcome..!!'}/>
+      <BrowserRouter>
+        <Navbar title={'The Grinch'} />
+        <Routes>
+          <Route
+            path="/"
+            element={<ItemListContainer greeting={'Welcome..!!'} />}
+          />
+          <Route
+            path="/category/:categoryId"
+            element={<ItemListContainer greeting={'New prducts'} />}
+          />
+          <Route
+            path="/Item/:itemId"
+            element={<ItemDetailContainer />}
+          />
+          <Route
+            path="*"
+            element={<h1>404 not found</h1>}
+          />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,51 +1,72 @@
 import CartWidget from '../CartWidget/CardWidget';
-import { Button, Center, SimpleGrid } from '@chakra-ui/react';
+import { Button, Flex, Box, Stack, Heading } from '@chakra-ui/react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = ({ title }) => {
   return (
-    <>
-      <Center 
-      bg={'teal'}
-      color={'white'}
-      fontWeight={'bold'}
-      fontSize={'1.5em'}
-      fontFamily={'Permanent Marker'}
-      >
-        <h1>{title}</h1>
-      </Center>
-      <Center>
-      <SimpleGrid columns={3}
-      spacing={7}>
-        
-        <Button
-          colorScheme="teal"
-          variant="ghost"
-          fontSize={'1.45em'}
-          fontWeight={'bold'}
+    <Box>
+      <Flex justifyContent="space-between">
+        <Box>
+          <Link to="/">
+            <Heading
+              bg="teal"
+              color="white"
+              fontWeight="bold"
+              fontFamily="Permanent Marker"
+              textAlign="center"
+              h="100%"
+              w="400px"
+              as="h1"
+            >
+              {title}
+            </Heading>
+          </Link>
+        </Box>
+        <Box>
+          <Stack
+            spacing={3}
+            direction="row"
+            align="center"
+            h="100%"
+            w="400px"
+          >
+            <Button
+              colorScheme="teal"
+              variant="ghost"
+              w="auto"
+              fontWeight="bold"
+              h="100%"
+            >
+              <NavLink to={`/category/trees`}>TREES</NavLink>
+            </Button>
+            <Button
+              colorScheme="teal"
+              variant="ghost"
+              w="auto"
+              fontWeight="bold"
+              h="100%"
+            >
+              <NavLink to={`/category/garlands`}>GARLANDS</NavLink>
+            </Button>
+            <Button
+              colorScheme="teal"
+              variant="ghost"
+              w="auto"
+              fontWeight="bold"
+              h="100%"
+            >
+              <NavLink to={`/category/decoration`}>DECORATION</NavLink>
+            </Button>
+          </Stack>
+        </Box>
+        <Box
+          w="325px"
+          h="100%"
         >
-          About
-        </Button>
-        <Button
-          colorScheme="teal"
-          variant="ghost"
-          fontSize={'1.45em'}
-          fontWeight={'bold'}
-        >
-          Home
-        </Button>
-        <Button
-          colorScheme="teal"
-          variant="ghost"
-          fontSize={'1.45em'}
-          fontWeight={'bold'}
-        >
-          Projects
-        </Button>
-      </SimpleGrid>
-      </Center>
-      <CartWidget />
-
-    </>
+          <CartWidget />
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
